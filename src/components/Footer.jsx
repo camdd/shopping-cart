6334cb61b0dab6f85b7b0c1b4aa86f16340c6aa0
Cam <camila.dambrosio95@gmail.com>
2023-12-18 18:29:16 -0300

    modificación en el z-index del carrito

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 
 import './Footer.css'
 
@@ -5,7 +6,7 @@ export function Footer () {
 
   return (
     <footer className='footer'>
-      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
+      <h4>Prueba técnica de React ⚛️ － <span><a href='http://github.com/camdd' target="_blank">Cam D&apos;Ambrosio</a></span></h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
