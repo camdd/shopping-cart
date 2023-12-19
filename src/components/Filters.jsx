16bcf7b252ac9141476059ffbbf24dfbc4797f5a
Cam <camila.dambrosio95@gmail.com>
2023-12-19 18:47:38 -0300

    new slider

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,51 +1,57 @@
+import React from 'react';
 import { useId } from 'react';
-import './Filters.css'
+import './Filters.css';
 import { useFilters } from '../hooks/useFilters';
+import * as Slider from '@radix-ui/react-slider';
 
 export function Filters() {
-    const {filters, setFilters} = useFilters()
+  const { filters, setFilters } = useFilters();
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
 
-    const minPriceFilterId = useId()
-    const categoryFilterId = useId()
+  const handleChangeMinPrice = (value) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      minPrice: value,
+    }));
+  };
 
-    const handleChangeMinPrice = (event) => {
-        setFilters(prevState => ({
-            ...prevState,
-            minPrice: event.target.value
-        }))
-    }
-
-    const handleChangeCategory = (event) => {
-        setFilters(prevState => ({
-            ...prevState,
-            category: event.target.value
-        }))
-    }
-
+  const handleChangeCategory = (event) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
 
   return (
     <section className="filters">
-      <div>
+<div>
         <label htmlFor={minPriceFilterId}>Min Price</label>
-        <input 
-        type="range" 
-        id={minPriceFilterId} 
-        min="0" 
-        max="1000" 
-        onChange={handleChangeMinPrice}
-        value={filters.minPrice}/>
+        <Slider.Root
+          className="SliderRoot"
+          value={[filters.minPrice]}
+          onValueChange={(newValue) => handleChangeMinPrice(newValue)}
+          min={0}
+          max={1000}
+          step={1}
+        >
+          <Slider.Track className="SliderTrack">
+            <Slider.Range className="SliderRange" />
+          </Slider.Track>
+          <Slider.Thumb className="SliderThumb" />
+        </Slider.Root>
         <span>${filters.minPrice}</span>
       </div>
 
-      
       <div>
         <label htmlFor={categoryFilterId}>Category</label>
-        <select  id={categoryFilterId} onChange={handleChangeCategory}>
-            <option value="all">All</option>
-            <option value="laptops">Laptops</option>
-            <option value="smartphones">Smartphones</option>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">All</option>
+          <option value="laptops">Laptops</option>
+          <option value="smartphones">Smartphones</option>
         </select>
       </div>
     </section>
   );
 }
+
