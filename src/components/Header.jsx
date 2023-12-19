42cfff14a99ee881aec6ba8ebea902407a5260b8
Cam <camila.dambrosio95@gmail.com>
2023-12-19 18:12:35 -0300

    created header and modified shopping cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
-import {Filters} from './Filters.jsx'
-import './Header.css'
+import { useEffect, useState } from "react";
+import "./Header.css";
 
 export function Header() {
-    return(
-        <header>
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
+  return (
+    <header className={visible ? '' : 'hidden'}>
+      <nav className="navbar container">
+        <ul className="navbar__menu">
+          <li className="navbar__item with-submenu">
             <h1>TECHNOSHOP</h1>
-        </header>
-    )
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
